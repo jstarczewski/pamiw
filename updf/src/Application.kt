fdcb4648fed8ec5eb04a25694f0bb7b1f789aebf
Jan Starczewski <jan.a.starczewski@gmail.com>
2019-11-18 20:31:17 +0100

    Add project setup files

---
+++ updf/src/Application.kt
@@ -0,0 +1,29 @@
+package com.jstarczewski.updf
+
+import io.ktor.application.*
+import io.ktor.auth.Authentication
+import io.ktor.auth.jwt.jwt
+import io.ktor.response.*
+import io.ktor.http.ContentType
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+fun Application.module() {
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }
+    server.start(wait = true)
+}
+
