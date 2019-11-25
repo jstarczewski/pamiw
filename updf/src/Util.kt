837c3fee3adca62f0d5cb21befaa9e494c3c4960
Jan Starczewski <jan.a.starczewski@gmail.com>
2019-11-25 22:50:29 +0100

    Create Util file with extension functions

---
+++ updf/src/Util.kt
@@ -0,0 +1,50 @@
+package com.jstarczewski.updf
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.yield
+import java.io.File
+import java.io.InputStream
+import java.io.OutputStream
+import java.lang.Exception
+
+fun <T> tryOrNull(block: () -> T): T? {
+    return try {
+        block()
+    } catch (exception: Exception) {
+        null
+    }
+}
+
+private const val extensionType = "idx"
+
+private val digitsOnlyRegex = "\\d+".toRegex()
+
+fun File.getAllIds(extension: String = extensionType) =
+    this.listFiles { f -> f.extension == extension && f.nameWithoutExtension.matches(digitsOnlyRegex) }
+        .mapTo(ArrayList()) { it.nameWithoutExtension.toLong() }
+
+suspend fun InputStream.copyToSuspend(
+    out: OutputStream,
+    bufferSize: Int = DEFAULT_BUFFER_SIZE,
+    yieldSize: Int = 4 * 1024 * 1024,
+    dispatcher: CoroutineDispatcher = Dispatchers.IO
+): Long {
+    return withContext(dispatcher) {
+        val buffer = ByteArray(bufferSize)
+        var bytesCopied = 0L
+        var bytesAfterYield = 0L
+        while (true) {
+            val bytes = read(buffer).takeIf { it >= 0 } ?: break
+            out.write(buffer, 0, bytes)
+            if (bytesAfterYield >= yieldSize) {
+                yield()
+                bytesAfterYield %= yieldSize
+            }
+            bytesCopied += bytes
+            bytesAfterYield += bytes
+        }
+        return@withContext bytesCopied
+    }
+}
