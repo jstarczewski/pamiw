55d6c14e297474af30df701ec14cbded36a2a338
Jan Starczewski <jan.a.starczewski@gmail.com>
2019-12-15 14:20:43 +0100

    Create injections for new components

--- updf/src/util/Injection.kt
+++ updf/src/util/Injection.kt
@@ -1,7 +1,9 @@
 package com.jstarczewski.updf.util
 
-import com.jstarczewski.updf.db.PdfDataSource
-import com.jstarczewski.updf.db.base.PdfDatabase
+import com.jstarczewski.updf.db.pdf.PdfDataSource
+import com.jstarczewski.updf.db.pdf.PdfDatabase
+import com.jstarczewski.updf.db.pub.PublicationDataSource
+import com.jstarczewski.updf.db.pub.PublicationDatabase
 import io.ktor.config.ApplicationConfig
 import io.ktor.util.KtorExperimentalAPI
 import java.io.File
@@ -10,7 +12,7 @@ import java.io.IOException
 object Injection {
 
     @KtorExperimentalAPI
-    fun provideUploadDir(config: ApplicationConfig): File {
+    fun providePdfUploadDir(config: ApplicationConfig): File {
         val uploadDirPath: String = config.property("upload.dir").getString()
         val uploadDir = File(uploadDirPath)
         if (!uploadDir.mkdirs() && !uploadDir.exists()) {
@@ -19,5 +21,19 @@ object Injection {
         return uploadDir
     }
 
-    fun provideLocalDataSource(uploadDir: File): PdfDataSource = PdfDatabase(uploadDir)
+    @KtorExperimentalAPI
+    fun providePublicationUploadDir(config: ApplicationConfig): File {
+        val uploadDirPath: String = config.property("publication.dir").getString()
+        val uploadDir = File(uploadDirPath)
+        if (!uploadDir.mkdirs() && !uploadDir.exists()) {
+            throw IOException("Failed to create directory ${uploadDir.absolutePath}")
+        }
+        return uploadDir
+    }
+
+    fun providePdfLocalDataSource(uploadDir: File): PdfDataSource =
+        PdfDatabase(uploadDir)
+
+    fun providePublicationsLocalDataSource(uploadDir: File): PublicationDataSource =
+        PublicationDatabase(uploadDir)
 }
