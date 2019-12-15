698482c8c6ef923212073be932fac318e652befe
Jan Starczewski <jan.a.starczewski@gmail.com>
2019-12-15 14:18:36 +0100

    Refactor pdf database

--- updf/src/db/pdf/PdfDataSource.kt
+++ updf/src/db/pdf/PdfDataSource.kt
@@ -1,10 +1,14 @@
-package com.jstarczewski.updf.db
+package com.jstarczewski.updf.db.pdf
+
+import com.jstarczewski.updf.db.Pdf
 
 interface PdfDataSource {
 
     fun getPdfById(id: Long): Pdf?
 
     fun savePdf(userName: String, title: String): Long
+
+    fun deletePdf(id: Long): Boolean
 
     fun getAllPdfFiles(): Sequence<Pdf>
 }
